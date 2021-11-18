0d528f23fcb0c6f428669ba53cb9c3c7e771787e
ruslan <josanr@gmail.com>
2021-11-18 12:00:00 +0300

    Task 3: changes after codeReview

--- src/test/java/ru/josanr/anagram/TestAnagram.java
+++ src/test/java/ru/josanr/anagram/TestAnagram.java
@@ -1,10 +1,9 @@
-package anagram;
+package ru.josanr.anagram;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import ru.josanr.anagram.Anagram;
 
 
 public class TestAnagram {
